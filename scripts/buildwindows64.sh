f2161a9e0a274e8567e70e889acfcd7bf8af9aec
TFinch <travisfinch01@gmail.com>
2020-09-04 22:19:13 -0400

    downgrade openssl for 18

--- scripts/buildwindows64.sh
+++ scripts/buildwindows64.sh
@@ -12,6 +12,7 @@ sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono
 sudo apt-get install libzmq3-dev -y
 sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl -y
+sudo apt-get install libssl1.0-dev -y
 
 sudo apt install g++-mingw-w64-x86-64 -y
 sudo update-alternatives --config x86_64-w64-mingw32-g++ -y # Set the default mingw32 g++ compiler option to posix. or choose 0 for deb8
