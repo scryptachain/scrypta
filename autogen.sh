913b089ff4af204daf4218ba93ca60db073cf9f2
TFinch <travisfinch01@gmail.com>
2020-09-17 15:25:11 -0400

    Test for cli integration

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,5 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
+sudo apt-get install libtool -y
 autoreconf --install --force --warnings=all
