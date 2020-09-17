2b977a0b92cf753941f278ae9f3ebe1899082fb6
TFinch <travisfinch01@gmail.com>
2020-09-17 15:27:41 -0400

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -6,5 +6,4 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
-sudo apt-get install libtool -y
 autoreconf --install --force --warnings=all
