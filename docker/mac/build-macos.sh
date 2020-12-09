ba657cfe852e7488ee46a3e87477a4fdf12fb242
turinglabs <sebastiano.cataudo@gmail.com>
2020-12-09 05:42:57 +0100

    QT changes / Docker Corrections

--- docker/mac/build-macos.sh
+++ docker/mac/build-macos.sh
@@ -7,3 +7,4 @@ docker run -d --name macos -p 42222:42222 -p 42223:42223 scrypta:macos
 docker cp macos:/opt/scrypta/src/lyrad  ./build/lyrad
 docker cp macos:/opt/scrypta/src/lyra-cli  ./build/lyra-cli
 docker cp macos:/opt/scrypta/src/qt/lyra-qt  ./build/lyra-qt
+docker cp macos:/opt/scrypta/Scrypta-Core.dmg  ./build/Scrypta-Core.dmg
