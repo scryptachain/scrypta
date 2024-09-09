d84836a9f8c7ea85391d4d88e10176629abbfa64
Gabriele-Romoli <gabriele.romoli@comm-it.it>
2024-09-09 14:21:56 +0200

    try fix

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@ set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
 ls -a
+cd build-aux
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
