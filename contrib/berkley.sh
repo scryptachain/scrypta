22fecf1b220fc9be72c84cd58c84619916cf3623
Gabriele-Romoli <gabriele.romoli@comm-it.it>
2024-09-10 17:39:14 +0200

    Fix install.sh

---
+++ contrib/berkley.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo add-apt-repository ppa:pivx/berkeley-db4
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED72260A3CDEA874
--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 #/bin/bash
 
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
+# Check if user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-get update
-sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y openssl1.0
 sudo apt-get install -y build-essential autoconf automake libboost-all-dev libleveldb-dev libgmp-dev libgmp3-dev libssl-dev libcurl4-openssl-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev autogen libtool git libevent-dev libprotobuf-dev
 sudo apt-get install -y curl g++ git-core pkg-config libtool faketime bsdmainutils mingw-w64 g++-mingw-w64 nsis zip ca-certificates python
 sudo apt-get install -y libzmq3-dev
 sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 sudo apt-get install -y libqrencode-dev 
 sudo apt-get install -y libssl1.0-dev
-git clone https://github.com/scryptachain/scrypta
-cd scrypta
+# Check if scrypta directory exists
+if [ ! -d "scrypta" ]; then
+    echo "Cloning LYRA repository..."
+    git clone https://github.com/Romoli-Gabriele/scrypta
+    cd scrypta
+else
+    echo "LYRA directory already exists. Skipping clone."
+    cd scrypta
+    git pull
+fi
 ./autogen.sh
 ./configure
 sudo make
