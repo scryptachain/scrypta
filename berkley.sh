37acae581c4d233bf14b5d7ff132a94de90a87d0
Gabriele-Romoli <gabriele.romoli@comm-it.it>
2024-09-16 17:29:27 +0200

    Fix berkley.sh

--- berkley.sh
+++ berkley.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 sudo add-apt-repository ppa:pivx/berkeley-db4
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED72260A3CDEA874
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
